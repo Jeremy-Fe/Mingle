e2859a1b93fc86bc18bff9a56735175135bb93d6
Shinwoocheol <snrn221@Naver.com>
2023-08-23 10:42:05 +0900

    Initial commit

---
+++ src/test/java/Mingle/MingleProject/MingleProjectApplicationTests.java
@@ -0,0 +1,14 @@
+package Mingle.MingleProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MingleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+
+}
