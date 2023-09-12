b89f2021ef3994bd0dd3a08db9e291c07eb89186
UngGiJung <jwg1047@gmail.com>
2023-09-12 14:31:50 +0900

    09.12 02:31 Mypage 작성중/ 마이페이지끝, 가입한 모임 작성 중

---
+++ src/main/java/Mingle/MingleProject/service/PostService.java
@@ -0,0 +1,32 @@
+package Mingle.MingleProject.service;
+
+import Mingle.MingleProject.Mapper.EntityDTOMapper;
+import Mingle.MingleProject.dto.PostDTO;
+import Mingle.MingleProject.entity.GatheringEntity;
+import Mingle.MingleProject.entity.PostEntity;
+import Mingle.MingleProject.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+    private final PostRepository postRepository;
+
+    public List<PostDTO> findPost(String logInId) {
+        List<PostEntity> postEntities = postRepository.findBypMId(logInId);
+        List<PostDTO> postDTOList = new ArrayList<>();
+        for (PostEntity postEntity: postEntities) {
+            postDTOList.add(EntityDTOMapper.entityToDTO(postEntity));
+            System.out.println(postDTOList);
+        }
+
+        return postDTOList;
+    }
+}
