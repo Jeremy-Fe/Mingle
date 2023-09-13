5587b5e471765b62ee03b5f8437287c97293a2b9
UngGiJung <jwg1047@gmail.com>
2023-09-13 16:01:05 +0900

    09.13 04:00 모임페이지 프로필사진 출력 완료

--- src/main/java/Mingle/MingleProject/repository/MemberRepository.java
+++ src/main/java/Mingle/MingleProject/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package Mingle.MingleProject.repository;
 
 import Mingle.MingleProject.entity.MemberEntity;
+import Mingle.MingleProject.entity.ScheduleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
